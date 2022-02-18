350a860939ff982c9de2d4a3bb360c61d8fc3b7d
Stas Sytnykov <stas82kak05@gmail.com>
2022-02-18 16:55:31 +0200

    add get data function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './sass/main.scss';
+import { fetchImage } from './js-module/get-data';
+
+const buttonSearch = document.querySelector('#search-form');
+
+const searchImg = event => {
+  event.preventDefault();
+  const {
+    elements: { searchQuery },
+  } = event.currentTarget;
+
+  console.log(searchQuery.value);
+};
+
+buttonSearch.addEventListener('submit', searchImg);
+
+fetchImage('nature')
+  .then(({ hits }) => {
+    console.log(hits);
+  })
+  .catch(error => console.log(error));
--- src/js-module/get-data.js
+++ src/js-module/get-data.js
@@ -1,1 +1,17 @@
 const axios = require('axios').default;
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '?key=25768905-c1ae5571e78baab059b11338b';
+
+function fetchImage(imgName) {
+  return fetch(
+    `${BASE_URL}${API_KEY}&q=${imgName}&image_type=photo&orientation=horizontal&safesearch=true`,
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+export { fetchImage };
