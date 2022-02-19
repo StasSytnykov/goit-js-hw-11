85d8156b52d5ac8bc9d2dd5237f3bf3fff3eac18
Stas Sytnykov <stas82kak05@gmail.com>
2022-02-19 14:33:58 +0200

    render markup

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import './sass/main.scss';
 import { fetchImage } from './js-module/get-data';
+import markupTamplate from './hbs-template/render-markup-gallery.hbs';
 
 const buttonSearch = document.querySelector('#search-form');
+const galleryContainer = document.querySelector('.gallery');
 
-const searchImg = event => {
+const onSearchImg = event => {
   event.preventDefault();
   const {
     elements: { searchQuery },
   } = event.currentTarget;
 
   console.log(searchQuery.value);
+
+  // renderMarkup({ hits });
+
+  fetchImage(searchQuery.value)
+    .then(({ hits }) => {
+      onRenderMarkupGallery(hits);
+      console.log(hits);
+    })
+    .catch(error => console.log(error));
+
+  // fetchImage(searchQuery.value)
+  //   .then(renderMarkup({ hits }))
+  //   .catch(error => console.log(error));
 };
 
-buttonSearch.addEventListener('submit', searchImg);
+const onRenderMarkupGallery = imgData => {
+  galleryContainer.innerHTML = markupTamplate(imgData);
+};
 
-fetchImage('nature')
-  .then(({ hits }) => {
-    console.log(hits);
-  })
-  .catch(error => console.log(error));
+buttonSearch.addEventListener('submit', onSearchImg);
